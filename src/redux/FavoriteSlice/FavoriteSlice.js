7148d420e3066a3f20abf97c6c64e78be534ad43
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-24 18:28:15 -0500

    change FavoriteSlice

--- src/redux/FavoriteSlice/FavoriteSlice.js
+++ src/redux/FavoriteSlice/FavoriteSlice.js
@@ -2,25 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     favorites: JSON.parse(localStorage.getItem("favorites")) || []
-
 };
 
 export const FavoriteSlice = createSlice({
-    name:"favorite",
+    name: "favorite",
     initialState,
-    reducers:{
+    reducers: {
         AddFavorite: (state, action) => {
-            const book = action.payload
-            state.favorites.push(book);
-            localStorage.setItem("favorites", JSON.stringify(state.favorites));
+            const book = action.payload;
+            const bookIsFavorited = state.favorites.find((fav) => fav.id === book.id);
+
+            if (!bookIsFavorited) {
+                state.favorites.push(book);
+                localStorage.setItem("favorites", JSON.stringify(state.favorites));
+            }
         },
         RemoveFavorite: (state, action) => {
             const bookId = action.payload;
-            state.favorites = state.favorites.filter(book => book.id !== bookId);
+            state.favorites = state.favorites.filter((book) => book.id !== bookId);
             localStorage.setItem("favorites", JSON.stringify(state.favorites));
         }
     }
-})
+});
 
-export const {AddFavorite, RemoveFavorite} = FavoriteSlice.actions;
+export const { AddFavorite, RemoveFavorite } = FavoriteSlice.actions;
 export default FavoriteSlice.reducer;
