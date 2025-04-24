805261b1eae1c889cc951c4fe49e2c937a925e5e
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-24 17:08:00 -0500

    add_components scaffholding

---
+++ src/data/data.jsx
@@ -0,0 +1,18 @@
+export const bookList = [
+    {
+      id: 1,
+      title: "1984",
+      author: "George Orwell",
+      year: 1949,
+      category: "Ciencia ficción",
+      synopsis: "A dystopian novel set in a totalitarian society ruled by Big Brother."
+    },
+    {
+      id: 2,
+      title: "Don Quijote",
+      author: "Miguel de Cervantes",
+      year: 1605,
+      category: "Clásico",
+      synopsis: "A nobleman becomes a knight and seeks adventures based on chivalric romances."
+    },
+ ]
