27a3014770f40bf972d46b37e2d51de51c1bcb20
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-24 18:32:10 -0500

    toggle favorite button

--- src/components/FavoriteButton/FavoriteButton.jsx
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,17 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
-import { AddFavorite } from "../../redux/FavoriteSlice/FavoriteSlice";
+import { AddFavorite, RemoveFavorite } from "../../redux/FavoriteSlice/FavoriteSlice";
 
-const FavoriteButton = ({bookInfo}) => {
+const FavoriteButton = ({ bookInfo }) => {
     const dispatch = useDispatch();
-    const favorites = useSelector((state)=> state.favorites.favorites)
+    const favorites = useSelector((state) => state.favorites.favorites);
 
-    const addToFavorites = (bookInfo) => {
-        dispatch(AddFavorite(bookInfo))
-    }
+    const favoriteMatch = favorites.find((fav) => fav.id === bookInfo.id);
+    const isFavorite = favoriteMatch; 
+    const toggleFavorite = () => {
+        if (isFavorite) {
+            dispatch(RemoveFavorite(bookInfo.id));
+        } else {
+            dispatch(AddFavorite(bookInfo));
+        }
+    };
 
-    return(
-        <button className="favorite-button" onClick={() => addToFavorites(bookInfo)}></button>
-    )
-}
+    return (
+        <button
+            className={"favorite-button"}
+            onClick={toggleFavorite}
+        >
+            {isFavorite ? "Remove from favorites" : "Add to favorites"}
+        </button>
+    );
+};
 
 export default FavoriteButton;
